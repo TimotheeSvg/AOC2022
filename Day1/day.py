5b351f0186ca75a3e3dc4b324375eb67401795e9
timothee_sauvage <tibo.desucre@gmail.com>
2022-12-02 19:31:48 +0100

    day1 up

--- Day1/day.py
+++ Day1/day.py
@@ -6,8 +6,6 @@ DAY = 1
 DEBUG = True
 s = get_input(DAY).strip()
 
-
-# PART 1 
 PART = 1
 ans = None
 
@@ -18,17 +16,13 @@ if(ans and not DEBUG):
 else:
     print(f"[DEBUG] Part 1 : {ans}")
 
-print("je suis le bot")
-#part 2
 PART = 2
 ans = 0
-
 tabnbr = list(map(sum,[[int(nbr)for nbr in tabNbrStr.strip('\n').split('\n')] for tabNbrStr in s.split('\n\n')]))
 
 for _ in range(3):
     ans +=max(tabnbr)
     tabnbr.remove(max(tabnbr))
-
 
 if(ans and not DEBUG):    
     submit(DAY, PART, ans)

94f7774a32e0498745545c3195cbaabd0db5b9a9
Timothée SAUVAGE <t.sauvage@lyon.ort.asso.fr>
2022-12-05 12:10:27 +0100

    Day5 up

---
+++ Day5/AOC.py
@@ -0,0 +1,43 @@
+# Advent of Code template by @MathisHammel
+
+import requests
+
+ # Put your session cookie in this variable
+
+
+AOC_COOKIE = "53616c7465645f5fb42e328cfd026ce1158b677468e7485467e4fc807b840b838977f63ec5b9dd06aa5819721a60ec1ac9146bb57a18953a318872262141cc3e"
+
+YEAR = '2022'
+
+def get_input(day):
+    req = requests.get(f'https://adventofcode.com/{YEAR}/day/{day}/input', headers={'cookie':'session='+AOC_COOKIE})
+    return req.text
+
+def get_example(day,offset=0):
+    req = requests.get(f'https://adventofcode.com/{YEAR}/day/{day}', headers={'cookie':'session='+AOC_COOKIE})
+    return req.text.split('<pre><code>')[offset+1].split('</code></pre>')[0]
+
+def submit(day, level, answer):
+    input(f'You are about to submit the follwing answer:\n>>>>>>>>>>>>>>>>> {answer}\nPress enter to continue or Ctrl+C to abort.')
+    data = {
+      'level': str(level),
+      'answer': str(answer)
+    }
+
+    response = requests.post(f'https://adventofcode.com/{YEAR}/day/{day}/answer', headers={'cookie':'session='+AOC_COOKIE}, data=data)
+    if 'You gave an answer too recently' in response.text:
+        print('VERDICT : TOO MANY REQUESTS')
+    elif 'not the right answer' in response.text:
+        if 'too low' in response.text:
+            print('VERDICT : WRONG (TOO LOW)')
+        elif 'too high' in response.text:
+            print('VERDICT : WRONG (TOO HIGH)')
+        else:
+            print('VERDICT : WRONG (UNKNOWN)')
+    elif 'seem to be solving the right level.' in response.text:
+        print('VERDICT : INVALID LEVEL')
+    else:
+        print('VERDICT : OK !')
+
+def ints(s):
+    return list(map(int, s.split()))
---
+++ Day5/day.py
@@ -0,0 +1,41 @@
+from AOC import * 
+import collections
+import math
+import itertools
+import time
+import random
+import pandas as pd
+import numpy as np
+
+
+DAY = 1
+DEBUG = True
+#s = get_input(DAY).strip()
+
+# PART 1 
+PART = 1
+ans = None
+
+#
+# WRITE HERE
+#
+
+
+if(ans and not DEBUG):    
+    submit(DAY, PART, ans)
+else:
+    print(f"[DEBUG] Part 1 : {ans}")
+
+
+#part 2
+PART = 2
+ans = 0
+
+#
+# WRITE HERE
+#
+
+if(ans and not DEBUG):    
+    submit(DAY, PART, ans)
+else:
+    print(f"[DEBUG] Part 2 : {ans}")
